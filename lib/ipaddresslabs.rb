f8672f2ab4f08dd4d72bb177d11a0052dedadd71
Nícolas Iensen <nicolas@engage.is>
2014-03-26 14:24:01 -0300

    Add Ipaddresslabs::locate method

--- lib/ipaddresslabs.rb
+++ lib/ipaddresslabs.rb
@@ -1,5 +1,9 @@
 require "ipaddresslabs/version"
+require "httparty"
 
 module Ipaddresslabs
-  # Your code goes here...
+  def self.locate ip
+    result = HTTParty.get("http://api.ipaddresslabs.com/iplocation/v1.7/locateip?key=#{ENV["IPADDRESSLABS_KEY"]}&ip=#{ip}&format=JSON")
+    JSON.parse(result.body)
+  end
 end
